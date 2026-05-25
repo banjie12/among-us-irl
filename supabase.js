a332f68a2f9fb452f3436f1f8e1f17a42e7d03ef
banjie12 <135499718+banjie12@users.noreply.github.com>
2026-05-25 10:03:23 +0100

    Create supabase.js for stage 1
    Communicate to Supabase using link and annon key

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js";
+
+export const supabase=createClient(
+    "https://vaehzkyshwrjrnscluro.supabase.co/",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhZWh6a3lzaHdyanJuc2NsdXJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2OTQ2MDAsImV4cCI6MjA5NTI3MDYwMH0.itUQecHikUhG9aBXjsQCI27wHKfFtfmHeO_RShG4bDg"
+);
